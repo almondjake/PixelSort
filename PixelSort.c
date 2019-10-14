50ce23b8d033d35fd4adfa632c027e7d0fed128b
almondjake <almondjake62@gmail.com>
2019-10-14 17:47:02 -0400

    comment

--- PixelSort.c
+++ PixelSort.c
@@ -1,19 +1,5 @@
 /*
- * A simple libpng example program
- * http://zarb.org/~gc/html/libpng.html
- *
- * Modified by Yoshimasa Niwa to make it much simpler
- * and support all defined color_type.
- *
- * To build, use the next instruction on OS X.
- * $ brew install libpng
- * $ clang -lz -lpng16 libpng_test.c
- *
- * Copyright 2002-2010 Guillaume Cottenceau.
- *
- * This software may be freely redistributed under the terms
- * of the X11 license.
- *
+
  */
 
 #include <stdlib.h>
